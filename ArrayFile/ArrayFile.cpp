b79ba934b0e2ee77ec705514b9f27139dfbf7ed8
YuriiLenchuk <lenchuk.yurii@chnu.edu.ua>
2023-03-01 00:36:31 +0200

    lab1

--- ArrayFile/ArrayFile.cpp
+++ ArrayFile/ArrayFile.cpp
@@ -1,23 +1,28 @@
-﻿ // ArrayFile.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+﻿// ArrayFile.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
 #include <iostream>
 #include <fstream>
 #include <ios>
 #include <vector>
-
+#include <algorithm>
 #include <time.h>
+
+
 
 using namespace std;
 
 typedef double* pDouble;
+const int MAX_SIZE = 200;
+
+int y;
 /*
 *   ConsoleInputArrayDouble
-*   
+*
 */
 int ConsoleInputSizeArray(const int sizeMax)
 {
-    int size = 0; 
+    int size = 0;
     do {
         cout << " Input size Array ( 0< 1 < " << sizeMax << " ) ";
         cin >> size;
@@ -31,7 +36,7 @@ int ConsoleInputSizeArray(const int sizeMax)
 int ConsoleInputArray(int sizeMax, double A[])
 {
     int size = ConsoleInputSizeArray(sizeMax);
-        for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) {
         cout << " Array[ " << i << "] "; cin >> A[i];
     }
     return size;
@@ -41,23 +46,36 @@ int ConsoleInputArray(int sizeMax, double A[])
 *   RndInputArrayDouble
 *
 */
-int RndInputArray(int sizeMax, double A[])
+int RndInputArray(int sizeMax, vector<int>& A)
 {
     int size = ConsoleInputSizeArray(sizeMax);
-    int r1=0, r2=0;
+    int r1 = 0, r2 = 0;
     srand(size);
 
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++, cout << endl) {
         r1 = rand();
-        r2 = rand();
-        A[i] = 100.0 * r1;
-        A[i] = A[i] / (1.0 + r2);
-        cout << A[i] << "   ";
+        r2 = rand() % 1;
+        A.push_back((r2) ? r1 : -r1);
+        cout << A[i] << ' ';
+    }
+    return size;
+}
+int RndInputArray(int sizeMax, vector<double>& A)
+{
+    int size = ConsoleInputSizeArray(sizeMax);
+    int r1 = 0, r2 = 0;
+    srand(size);
+
+    for (int i = 0; i < size; i++, cout << endl) {
+        r1 = rand();
+        r2 = rand() % 1;
+        A.push_back((r2) ? r1 : -r1);
+        cout << A[i] << ' ';
     }
     return size;
 }
 
-int ConsoleInputDynamicArrayNew(int sizeMax, pDouble &pA)
+int ConsoleInputDynamicArrayNew(int sizeMax, pDouble& pA)
 {
     int size = ConsoleInputSizeArray(sizeMax);
     pA = new double[size];
@@ -79,73 +97,151 @@ int ConsoleInputDynamicArray_calloc(int sizeMax, pDouble& pA)
     return size;
 }
 
-void ConsoleInputVector(int sizeMax, vector<double> &A)
+void ConsoleInputVector(int sizeMax, vector<double>& A)
 {
     int size = ConsoleInputSizeArray(sizeMax);
     double d;
     for (int i = 0; i < size; i++) {
         cout << " Array[ " << i << "] "; cin >> d; A.push_back(d);
     }
-    return ;
+    return;
 }
+
+void ConsoleInputVector(int sizeMax, vector<int>& A)
+{
+    int size = ConsoleInputSizeArray(sizeMax);
+    int d;
+    for (int i = 0; i < size; i++) {
+        cout << " Array[ " << i << "] "; cin >> d; A.push_back(d);
+    }
+    return;
+}
+
 
 
 /*
-*  WriteArrayTextFile 
+*  WriteArrayTextFile
 *
 */
 
-void WriteArrayTextFile(int n, double *arr, const char *fileName )
+void WriteArrayTextFile(int n, double* arr, const char* fileName)
 {
-    ofstream fout(fileName);
-    if (fout.fail()) return;
+    ofstream fout(fileName, ios::app);
+    if (fout.fail())
+    {
+        cout << "Erorr"; return;
+    }
     fout << n << endl;
     for (int i = 0; i < n; i++)
         fout << arr[i] << "   ";
-    fout.close(); //
+    fout.close();
 }
+void WriteArrayTextFile(int n, vector<int>& arr, const char* fileName)
+{
+    ofstream fout(fileName, ios::app);
+    if (fout.fail())
+    {
+        cout << "Erorr"; return;
+    }
+    fout << n << endl;
+    for (int i = 0; i < n; i++)
+        fout << arr[i] << "   ";
+    fout.close();
+}
+void WriteArrayTextFile(int n, vector<double>& arr, const char* fileName)
+{
+    ofstream fout(fileName, ios::app);
+    if (fout.fail())
+    {
+        cout << "Erorr"; return;
+    }
+    fout << n << endl;
+    for (int i = 0; i < n; i++)
+        fout << arr[i] << "   ";
+    fout << '\n';
+    fout.close();
+}
+
 /*
 *  ReadArrayTextFile
 *
 */
 
 
-int ReadArrayTextFile(int n, double* arr, const char* fileName)
+int ReadArrayTextFile(double* arr, const char* fileName)
 {
     int size;
     ifstream fin(fileName);
-    if (fin.fail()) return 0;
+    if (!fin.is_open())
+    {
+        cout << "Erorr text"; return 0;
+    }
     fin >> size;
     if (size <= 0) return 0;
-    if (size > n) size = n;   
-    for (int i = 0; i < n; i++)
-       fin>> arr[i];
+    for (int i = 0; i < size; i++)
+        fin >> arr[i];
     fin.close();
     return size;
-    
+}
+int ReadArrayTextFile(vector<int>& arr, const char* fileName)
+{
+    int size;
+    double buff = 0;
+    ifstream fin(fileName);
+    if (!fin.is_open())
+    {
+        cout << "Erorr text"; return 0;
+    }
+    fin >> size;
+    if (size <= 0) return 0;
+    for (int i = 0; i < size; i++)
+    {
+        fin >> buff;
+        arr.push_back(buff);
+    }
+    fin.close();
+    return size;
+}
+int ReadArrayTextFile(vector<double>& arr, const char* fileName)
+{
+    int size;
+    double buff = 0;
+    ifstream fin(fileName);
+    if (!fin.is_open())
+    {
+        cout << "Erorr text"; return 0;
+    }
+    fin >> size;
+    if (size <= 0) return 0;
+    for (int i = 0; i < size; i++)
+    {
+        fin >> buff;
+        arr.push_back(buff);
+    }
+    fin.close();
+    return size;
 }
 
 
-void WriteArrayBinFile(int n, double* arr, const char* fileName)
+void WriteArrayBinFile(int n, vector<double>& arr, const char* fileName)
 {
     //ios_base
-    ofstream bfout(fileName, ios_base::binary);
+    ofstream bfout(fileName, ios::binary | ios::app);
     if (bfout.fail()) return;
-    bfout.write((const char*)&n, sizeof(int));
-    std::streamsize  cn = static_cast<std::streamsize>(n) *sizeof(double);
-    bfout.write((const char*)arr, cn);
+    bfout.write((char*)&n, sizeof(int));
+    bfout.write((char*)arr.data(), n * sizeof(arr[0]));
     bfout.close();
 }
 
-int ReadArrayBinFile(int n, double* arr, const char* fileName)
+int ReadArrayBinFile(int n, vector<double>& arr, const char* fileName)
 {
-    int size=0;
+    int size = 0;
     ifstream bfin(fileName, ios_base::binary);
     if (bfin.fail()) return 0;
     bfin.read((char*)&size, sizeof(int));
     if (size <= 0) return 0;
     if (size > n) size = n;
-    bfin.read((char*)arr, static_cast<std::streamsize>(size) * sizeof(double));
+    bfin.read((char*)&arr, n * sizeof(double));
     bfin.close();
     // ssdhs
     return size;
@@ -157,14 +253,15 @@ void ShowMainMenu()
     cout << "    1.  Task 1  \n";
     cout << "    2.  Task 2  \n";
     cout << "    3.  Task 3  \n";
-  }
+    cout << "    4.  Exit  \n";
+}
 
 void MenuTask()
 {
     cout << "     Menu Task   \n";
     cout << "    1.  Local array  \n";
     cout << "    2.  Dynamic array 1 \n";
-    cout << "    3.  Dynamic array 2  new \n"; 
+    cout << "    3.  Dynamic array 2  new \n";
     cout << "    4.  Dynamic array : vector \n";
     cout << "    5.  Exit \n";
 }
@@ -175,106 +272,108 @@ void MenuInput()
     cout << "    1.  Console all \n";
     cout << "    2.  Console - size, array - random \n";
     cout << "    3.  File 1.txt \n";
-    cout << "    4.  bb    \n";
-    cout << "    5.  Exit \n";
+    cout << "    4.  Exit \n";
 }
 
 
-/*
-* Задано одновимірний масив А розміру 2N. 
-* Побудувати два масиви В і С розміру N, 
-* включивши  у масив В елементи масиву А з парними індексами,
-* а у С - з непарними.
-*****************
-*  A - in 
-*  B, C - out 
-*/
-void  TestVariant(int N, double* A, double* B, double* C) {
-    for (int i = 0; i < N; i++) {
-        B[i] = A[2 * i];
-        C[i] = A[2 * i + 1];
+void Task1()
+{
+    vector<double> a, b;
+    if (y == 1) ConsoleInputVector(MAX_SIZE, a);
+    else if (y == 2) RndInputArray(MAX_SIZE, a);
+    else if (y == 3) ReadArrayTextFile(a, "1.txt");
+    else
+    {
+        cout << "Error" << endl;
+        return;
     }
+    copy_if(a.begin(), a.end(), back_inserter(b), [](double i) {return i < 0; });
+    for (int i = 0; i < b.size(); i++) cout << "B = " << b[i] << ' ';
+    WriteArrayTextFile(b.size(), b, "rez.txt");
+    WriteArrayBinFile(b.size(), b, "BinaryRez.bin");
+    cout << endl;
+    return;
 }
-/*
-*  Task  Var
-* 
-* 
-*/
-void TaskV()
+void Task2()
 {
-    char ch = '5';
-    do {
-        system("cls");
-        MenuTask();
-        ch = getchar();
-        getchar();
-            switch (ch) {
-             case '1': cout << " 1 "; break;
-             case '2': cout << " 2 "; break;
-            //
-            case '5': return;
-            }
-        cout << " Press any key and enter\n";
-        ch = getchar();
-        } while (ch != 27);
-    
+    vector<double> a, b;
+    if (y == 1) ConsoleInputVector(MAX_SIZE, a);
+    else if (y == 2) RndInputArray(MAX_SIZE, a);
+    else if (y == 3) ReadArrayTextFile(a, "1.txt");
+    else
+    {
+        cout << "Error" << endl;
+        return;
+    }
+    copy_if(a.begin(), a.end(), back_inserter(b), [](double i) {return i < 0; });
+    double T;
+    cout << "Enter T: "; cin >> T;
+    auto iter = find(b.begin(), b.end(), T);
+    ofstream file("rez.txt", ios::app);
+    double rez = *max_element(b.begin(), iter);
+    cout << rez << endl;
+    file << rez << '\n';
+    file.close();
+    WriteArrayBinFile(b.size(), b, "BinaryRez.bin");
+    cout << endl;
+    return;
 }
-
-void ArrayLocal()
+void Task3()
 {
-    double A[1000], B[500], C[500];
-    int n;
-    char ch = '5';
-    do {
-        system("cls");
-        MenuTask();
-        ch = getchar();
-        getchar();
-        switch (ch) {
-        case '1': cout << " 1 "; break;
-        case '2': cout << " 2 "; break;
-            //
-        case '5': return;
-        }
-        cout << " Press any key and enter\n";
-        ch = getchar();
-    } while (ch != 27);
-
+    vector<double> A, B;
+    if (y == 1) ConsoleInputVector(MAX_SIZE, A);
+    else if (y == 2) RndInputArray(MAX_SIZE, A);
+    else if (y == 3) ReadArrayTextFile(A, "1.txt");
+    else
+    {
+        cout << "Error" << endl;
+        return;
+    }
+    copy_if(A.begin(), A.end(), back_inserter(B), [A](double i) {return (bool)(count(A.begin(), A.end(), i) - 1); });
+    sort(B.begin(), B.end());
+    auto last = unique(B.begin(), B.end());
+    B.erase(last, B.end());
+    for (int i = 0; i < B.size(); i++, cout << ((i % 5 == 0 ? '\n' : ' '))) cout << B[i];
+    WriteArrayTextFile(B.size(), B, "rez.txt");
+    WriteArrayBinFile(B.size(), B, "BinaryRez.bin");
+    cout << endl;
+    return;
 }
 
+void Menu()
+{
+    int x;
+    ShowMainMenu();
+    cin >> x;
+    if (x == 1)
+    {
+        MenuInput();
+        cin >> y;
+        Task1();
+        Menu();
+    }
+    else if (x == 2)
+    {
+        MenuInput();
+        cin >> y;
+        Task2();
+        Menu();
+    }
+    else if (x == 3)
+    {
+        MenuInput();
+        cin >> y;
+        Task3();
+        Menu();
+    }
+    else if (x == 4) { return; }
+    else Menu();
+}
 
 int main()
-{ 
-    
-    
-    
-    const int MAX_SIZE = 560;
-    std::cout << "Hello World!\n";
-    ShowMainMenu();
-    /*
-    double A[MAX_SIZE], B[MAX_SIZE],C[MAX_SIZE];
-    int n,m;
-    n = RndInputArray(MAX_SIZE, A);
-    WriteArrayTextFile(n, A, "1.txt");
-    m = ReadArrayTextFile(MAX_SIZE, B, "1.txt");
-    cout << " \n m= " << m << endl;
-    for (int i = 0; i < m; i++)
-        cout << B[i] << "   ";
-    WriteArrayBinFile(n, A, "1.bin");
-    m = ReadArrayBinFile(MAX_SIZE, C, "1.bin");
-    cout << " \n m= " << m << endl;
-    for (int i = 0; i < m; i++)
-        cout << C[i] << "   ";
-    cout << "\n  Vector \n";
-    vector<double> vA;
-    ConsoleInputVector(MAX_SIZE, vA);
-    for (auto v : vA) {
-        cout << v << "   ";
-    }
-*/
-    TaskV();
-    return 1;
-
+{
+    Menu();
+    return 0;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
